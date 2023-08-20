8521b3eb9d4e2a1adf616fe7c1439c62e0f3a2c3
dkckhaudhary033 <d.k.chaudhary033@gmail.com>
2023-08-20 17:37:15 +0530

    'Java-Program-20Aug23'

---
+++ Pattern.java
@@ -0,0 +1,12 @@
+public class Pattern {
+   
+    public static void main(String[] args) {
+        System.out.println("Kya hall haiṇ");
+        for (i=1; i<=5; i++){
+            for (j=1; j<=3; j++){
+              System.out.print("*");
+            }
+              System.out.println();
+        }
+    }
+}
